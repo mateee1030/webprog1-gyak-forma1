c421089de679104f08f723e62ea41b5cf335ec93
mateee1030 <matekovacs1030@gmail.com>
2026-05-18 13:30:03 +0200

    Add files via upload

---
+++ includes/config.inc.php
@@ -0,0 +1,34 @@
+<?php
+$ablakcim = array(
+    'cim' => 'Forma-1 Adatbázis'
+);
+
+$fejlec = array(
+    'kepforras' => 'images/logo.png',
+    'kepalt' => 'Forma-1 logó',
+    'cim' => 'Forma-1 Adatbázis',
+    'motto' => 'Web Programozás 1 beadandó'
+);
+
+$lablec = array(
+    'copyright' => 'Copyright ' . date("Y") . '.',
+    'ceg' => 'Forma-1 Projekt'
+);
+
+$oldalak = array(
+    '/'          => array('fajl' => 'fooldal',   'szoveg' => 'Főoldal',   'menun' => array(1,1)),
+    'kepek'      => array('fajl' => 'kepek',     'szoveg' => 'Képek',     'menun' => array(1,1)),
+    'kapcsolat'  => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+    'pilota'     => array('fajl' => 'pilota',    'szoveg' => 'CRUD',      'menun' => array(1,1)),
+    'uzenetek'   => array('fajl' => 'uzenetek',  'szoveg' => 'Üzenetek',  'menun' => array(0,1)),
+    'belepes'    => array('fajl' => 'belepes',   'szoveg' => 'Belépés',   'menun' => array(1,0)),
+    'kilepes'    => array('fajl' => 'kilepes',   'szoveg' => 'Kilépés',   'menun' => array(0,1)),
+    'belep'      => array('fajl' => 'belep',     'szoveg' => '',          'menun' => array(0,0)),
+    'regisztral' => array('fajl' => 'regisztral','szoveg' => '',          'menun' => array(0,0))
+);
+
+$hiba_oldal = array(
+    'fajl' => '404',
+    'szoveg' => 'A keresett oldal nem található!'
+);
+?>
