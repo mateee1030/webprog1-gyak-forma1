a19eb82349c82d96a25d495aa99b6b474a084071
mateee1030 <matekovacs1030@gmail.com>
2026-05-18 13:29:07 +0200

    Delete views directory

--- views/layout_bottom.php
+++
@@ -1,6 +0,0 @@
-</main>
-<footer>
-  <p>Készítette: <strong>Kovács Máté</strong> (FA98OA) &amp; <strong>Rácz Antal</strong> (XXA3HN) | Web programozás-1 Gyakorlat Beadandó | 2026</p>
-</footer>
-</body>
-</html>
